d7ddbfec40f157af30e93c1c9a6b434fd689fc6f
Samnoyal S <36767665+Samnoyal@users.noreply.github.com>
2023-11-27 15:51:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const path=require('path');
-//PORT
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+
+app.use(cors(corsOptions))
 app.use(express.static('public'));
 
-//DBConnection
-const connectDB = require('./Config/db');
+const connectDB = require('./config/db');
 connectDB();
 
-//Engine
-app.set('views',path.join(__dirname,'/views'));
-app.set('view engine','ejs');
+app.use(express.json());
 
-//Routes
-app.use('/api/files',require('./routes/files'));
-app.use('/files', require('./routes/show'))
-app.use('/files/download',require('./routes/download'));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
-app.listen(PORT, ()=>{
-    console.log(`Listening on port ${PORT}`);
-})
+// Routes 
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+
+const sslServer= https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+},app);
+
+//app.listen(PORT, console.log(`Listening on port ${PORT}.`));
+sslServer.listen(PORT, console.log(`Listening on port ${PORT}.`));
